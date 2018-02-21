186721d00744608ab6d34f1d18495342ef4e1f47
seanng <shonum@gmail.com>
2018-02-21 14:38:25 +0800

    feat: Change background wallpaper color

--- app/containers/App/AppWrapper.js
+++ app/containers/App/AppWrapper.js
@@ -5,7 +5,7 @@ const AppWrapper = styled.div`
   width: 100vw;
   min-height: 100vh;
   max-width: 100vw;
-  background-color: ${colors.lightGray};
+  background-color: ${colors.wallpaper};
 `;
 
 export default AppWrapper;

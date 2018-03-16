5dcb8c91960bbbd9d0d8c04febbfa73139a8b410
seanng <shonum@gmail.com>
2018-03-16 20:26:10 +0800

    refactor: fetchStays

--- server/io/hotelActions/fetchStays.js
+++ server/io/hotelActions/fetchStays.js
@@ -1,24 +1,21 @@
-const staysService = require('../../services/stays');
+const { fetchHotelHistory } = require('../../services/stays');
 const { reply } = require('../helpers');
+
+const handleSuccess = (client, stays) =>
+  reply(client, {
+    type: 'app/PastStays/FETCH_STAYS_SUCCESS',
+    stays,
+  });
+
+const handleFail = (client, error) =>
+  reply(client, {
+    type: 'app/PastStays/FETCH_STAYS_ERROR',
+    error,
+  });
 
 module.exports = (client, action) =>
   new Promise((resolve, reject) => {
-    staysService
-      .fetchHotelHistory(action.hotelId)
-      .then(stays => {
-        resolve(
-          reply(client, {
-            type: 'app/PastStays/FETCH_STAYS_SUCCESS',
-            stays,
-          })
-        );
-      })
-      .catch(error => {
-        reject(
-          reply(client, {
-            type: 'app/PastStays/FETCH_STAYS_ERROR',
-            error,
-          })
-        );
-      });
+    fetchHotelHistory(action.hotelId)
+      .then(stays => resolve(handleSuccess(client, stays)))
+      .catch(error => reject(handleFail(client, error)));
   });

9e9f267bcd1731b6b0cdee1374460ac33195fef9
seanng <shonum@gmail.com>
2018-04-23 13:13:52 +0800

    feat: create routes for getting hotel employees and delete employee

--- server/routes/employee/index.js
+++ server/routes/employee/index.js
@@ -3,7 +3,8 @@ const controller = require('./controller');
 const handleTryCatch = require('../../utils/handleTryCatch');
 
 routes.post('/', handleTryCatch(controller.addEmployee));
-routes.get('/', handleTryCatch(controller.getEmployee));
+routes.get('/:hotelId', handleTryCatch(controller.getHotelEmployees));
 routes.put('/', handleTryCatch(controller.putEmployee));
+routes.delete('/:id', handleTryCatch(controller.deleteEmployee));
 
 module.exports = routes;
